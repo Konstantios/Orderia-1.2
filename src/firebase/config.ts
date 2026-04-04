cb99c417eed82382d0e118ece7fb57d2d6478cb5
Konstantios Batsiolas <konstantios.batsiolas@gmail.com>
2026-04-04 13:52:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7324950597-c1440",
+  "appId": "1:757566827736:web:f2647238783f23494fbbad",
+  "apiKey": "AIzaSyC2XkuMFUWWZLyeY6z36hQx52dUwYJVVQs",
+  "authDomain": "studio-7324950597-c1440.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "757566827736"
+};
